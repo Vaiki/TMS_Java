3b71f09194eb5eef3947a233b5beacc0b2b15b24
vaiki <lifeotworld@gmail.com>
2022-03-12 21:17:52 +0300

    Task 5

---
+++ src/com/company/animal/Vet.java
@@ -0,0 +1,22 @@
+package com.company.animal;
+
+public class Vet {
+    void treatAnimal(Animal animal) {
+        System.out.println("Eat " + animal.food + "\nLives on " + animal.location+"\n");
+    }
+
+    public static void main(String[] args) {
+
+        Animal[] any = {new Cat("fish", "Gagarina st. 17"),
+                new Dog("meat", "Masherova st. 33"),
+                new Horse("hay", "Ratomka village")};
+
+
+
+        for (Animal animal : any) {
+           new Vet().treatAnimal(animal);
+
+        }
+    }
+
+}
